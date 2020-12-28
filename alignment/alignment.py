4a5a829309df9458c8eb2a934a9ce96256ab57b2
Leandro Furlam Turi <leandrofturi@gmail.com>
2020-12-28 15:56:09 -0300

    ajustes no incremento

--- alignment/alignment.py
+++ alignment/alignment.py
@@ -5,26 +5,24 @@ from scipy.interpolate import lagrange
 path = "alignment/ex/entrada.csv"
 df = pd.read_csv(path)
 
-X = df['x'].values
-Y = df['s'].values
-h = 0.5
+dt = align(df, 'x', 's', 0.5)
 
-dt = align(X, Y, h)
-
-def align(X, Y, h):
+def align(df, x, y, h):
+    X = df[x].values
+    Y = df[y].values
     i = 0
     j = 1
-    nrow = len(df.index)
+    nrow = len(X)
     A = [[X[0], Y[0]]]
     while i < nrow and j < nrow:
         if X[j] == A[-1][0] + h:
             A.append([X[j], Y[j]])
         elif X[j] > A[-1][0] + h:
-            L = lagrange(X[i:j+1], Y[i:j+1]) # L = lagrange(X[i:j+1].values, Y[i:j+1].values) if is pd.Series
+            L = lagrange(X[i:j+1], Y[i:j+1])
             A.append([A[-1][0] + h, L(A[-1][0] + h)])
         else:
             j = j + 1
-            if A[-1][0] - X[i] > h:
+            if X[i+1] >= A[-1][0]:
                 i = i + 1
-    dt = pd.DataFrame(A, columns=['x','y'])
+    dt = pd.DataFrame(A, columns=[x, y])
     return dt
